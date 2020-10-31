23b8a61bc8529f750691e36fda0e2b13d171b4a0
Dublin Anondson <Stratophone2112@gmail.com>
2020-10-31 09:55:46 -0500

    wip: added language selection feature & a pill component (#2)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
+<!-- TODO Create a section with the title, a short (1-2 sentence) mention of what this site is,  -->
+<h1> Welcome to PR Practice </h1>
+<p> Need something to review? Click the button to get a random PR to look at, if you want language specific, select some languages below. </p>
   <PRSearch />
+  <LanguageSelection />
 </template>
 
 <script>
 import PRSearch from './features/PRSearch';
+import LanguageSelection from './features/LanguageSelection';
 
 export default {
   name: 'App',
   components: {
     PRSearch,
+    LanguageSelection,
   }
 }
 </script>
